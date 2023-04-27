6ae2658e8c119351e05aaa0b77bd2ad069bd1e12
Matthiew Purple <matthiewpurple@hotmail.com>
2023-04-27 14:49:05 +0200

    new name

--- EscapeOnHard06.cs
+++ EscapeOnHard06.cs
@@ -2,10 +2,9 @@
 using HarmonyLib;
 using Il2Cpp;
 using escape_on_hard_06;
-using Random = System.Random;
 using Il2Cppnewbattle_H;
 
-[assembly: MelonInfo(typeof(EscapeOnHard06), "Escape on Hard (ver. 0.6)", "1.0.0", "Matthiew Purple")]
+[assembly: MelonInfo(typeof(EscapeOnHard06), "Escape on Hard [normal odds] (ver. 0.6)", "1.0.0", "Matthiew Purple")]
 [assembly: MelonGame("アトラス", "smt3hd")]
 
 namespace escape_on_hard_06;
